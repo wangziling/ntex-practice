9f44a274ccbbe69ddece3bc28b0107b6cdb3f4b0
王子凌 <sling1994@live.com>
2024-04-07 15:57:07 +0800

    chore(proc_macros): enhance the impl of the `web_view_template` proc macro

--- crates/proc_macros/src/view_template.rs
+++ crates/proc_macros/src/view_template.rs
@@ -16,10 +16,43 @@ pub fn impl_attr_web_view_template(_args: TokenStream, mut ast: ItemStruct) -> T
         });
     }
 
-    let gen = quote! {
+    let struct_name = &ast.ident;
 
+    let gen = quote! {
         #[derive(::sailfish::TemplateOnce, Default)]
         #ast
+
+        impl ::web_core::view_template::ViewTemplate for #struct_name {
+            fn title(&self) -> &str {
+                &self._base.title
+            }
+
+            fn description(&self) -> &str {
+                &self._base.description
+            }
+
+            fn language(&self) -> &str {
+                &self._base.language
+            }
+
+            fn set_title(&mut self, title: String) -> &mut Self {
+                self._base.title = title;
+
+                self
+            }
+
+            fn set_description(&mut self, description: String) -> &mut Self {
+                self._base.description = description;
+
+                self
+            }
+
+            fn set_language(&mut self, language: String) -> &mut Self {
+                self._base.language = language;
+
+                self
+            }
+        }
     };
 
     gen.into()
