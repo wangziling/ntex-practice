8a2a25cfaacb77f90d71fecb7aa0f2d333a2a6b8
王子凌 <sling1994@live.com>
2024-04-09 17:17:19 +0800

    feat(cache): enable a new crate named `cache`

---
+++ crates/cache/src/lib.rs
@@ -0,0 +1,22 @@
+#[macro_use]
+extern crate tracing;
+
+use web_core::prelude::*;
+
+mod distribute;
+
+pub mod prelude {
+    pub use crate::distribute::{
+        prelude::*, DistributeCache, DistributeCacheConfig, DistributeCacheExtension, DistributeCacheGlobal,
+        DistributeCacheKey,
+    };
+}
+
+/// Distribute cache can only be accessed in `app_state`.
+pub async fn generate_distribute_cache(
+    config: distribute::DistributeCacheConfig,
+) -> Result<distribute::DistributeCacheGlobal> {
+    debug!("Connecting to the distribute cache.");
+
+    distribute::generate(config).await
+}
