665ab5e2ef71305bd4bf8fb30615ef7b01d679b4
王子凌 <sling1994@live.com>
2024-05-06 23:01:22 +0800

    chore(www): enhance the event about enabling `tls`

--- crates/www/src/utils/server.rs
+++ crates/www/src/utils/server.rs
@@ -2,11 +2,11 @@ use std::{fs::File, io::BufReader, path::Path};
 use web_core::prelude::*;
 
 #[cfg(feature = "tls-rustls")]
-pub fn generate_tls_openssl_config<T>(key_file_path: T, cert_file_path: T) -> Result<rustls::ServerConfig>
+fn __generate_tls_openssl_config<T>(key_file_path: T, cert_file_path: T) -> Result<rustls::ServerConfig>
 where
     T: AsRef<Path>,
 {
-    // load ssl keys
+    // Load ssl keys.
     let mut key_file = BufReader::new(File::open(key_file_path.as_ref())?);
     let key_der = rustls_pemfile::private_key(&mut key_file)?.ok_or_else(|| anyhow!("Failed to load ssl key file."))?;
 
@@ -14,4 +14,16 @@ where
     let cert_chain = rustls_pemfile::certs(&mut cert_file).map(|c| c.unwrap()).collect();
 
     Ok(rustls::ServerConfig::builder().with_no_client_auth().with_single_cert(cert_chain, key_der)?)
+}
+
+#[cfg(feature = "tls-rustls")]
+pub fn generate_tls_openssl_config<T>(key_file_path: T, cert_file_path: T) -> Result<rustls::ServerConfig>
+where
+    T: AsRef<Path>,
+{
+    __generate_tls_openssl_config(key_file_path, cert_file_path).map_err(|e: anyhow::Error| {
+        eprintln!("Tls Error:{}", Into::<rustls::Error>::into(rustls::CertificateError::BadSignature));
+
+        e
+    })
 }
